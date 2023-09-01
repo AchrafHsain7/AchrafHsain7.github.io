8deba1b2f0a8c7db3f967d0dabf35aef66f3f0fe
AHProgramer <95654726+AHProgramer@users.noreply.github.com>
2023-09-01 23:12:27 +0100

    SlideShow finally implemented!

--- index.js
+++ index.js
@@ -43,4 +43,59 @@ document.addEventListener('DOMContentLoaded', function(){
     // Call it initially as well
     handleWidthChange(width);
         
+
+    //slideshow 
+    let slides = document.querySelectorAll('.slide');
+            let btns = document.querySelectorAll('.btn');
+            let currentSlide = 1;
+
+            //manual Navigation
+            var manualNav = function(manual){
+                slides.forEach((slide)=>{
+                    slide.classList.remove('active');
+                });
+
+                btns.forEach((btn) =>{
+                    btn.classList.remove('active');
+                });
+
+                slides[manual].classList.add('active');
+                btns[manual].classList.add('active');
+            }
+
+            btns.forEach((btn, i) => {
+                btn.addEventListener('click', ()=>{
+                    manualNav(i);
+                    currentSlide = i;
+                })
+            })
+
+    //autoplay slideShow
+    var repeat = function(activeClass){
+
+        let active = document.getElementsByClassName('active');
+        let i = 1;
+
+        var repeater = ()=>{
+            setTimeout(()=>{
+                [...active].forEach((activeSlide) => {
+                    activeSlide.classList.remove('active');
+                })
+
+                slides[i].classList.add('active');
+                btns[i].classList.add('active');
+                i++;
+
+                if(slides.length === i){
+                    i=0;
+                }
+                if(i >= slides.length){
+                    return;
+                }
+                repeater();
+            }, 10000);
+        }
+        repeater();
+    }
+    repeat();
 })
